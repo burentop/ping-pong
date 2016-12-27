d74c20c555fe1c51d0d5b7ded0707b1f8663fc34
Brent Perry <burentoperii@gmail.com>
2016-12-27 14:54:05 -0600

    complete all tests and build user interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,13 @@
+import java.io.Console;
+import java.util.List;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("I'm a ping-pong application!");
+    Console myConsole = System.console();
+    System.out.println("Enter a number:");
+    int userNumber = Integer.parseInt(myConsole.readLine());
+    PingPong pingPong = new PingPong();
+    List<Object> pingPongResult = pingPong.runPingPong(userNumber);
+    System.out.println(pingPongResult);
   }
 }
